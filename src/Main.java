0aba5c5bdd1256de773cb29b4462e7f884db063f
TretyakovaA <putinvivo@bk.ru>
2022-07-14 19:29:39 +0300

    draft

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PersonService personService = new PersonService ();
+        Person sarah = new Person ("Sarah", 30);
+        Person john = new Person("John","13");
+        sarah.child = john;
+        john = new Person("Alternativajoh", 30);
+        john = null;
+
+
+
+        personService.setAgeToEighteen (sarah);
+        if (sarah.child != null){
+            if (sarah.child.getName() != null) {
+                System.out.println("sarah.child.getName().length() = " + sarah.child.getName().length());
+            } else {
+                System.out.println(0);
+            }
+        }
+        System.out.println( "sarah.getAge () = "+ sarah.getAge);
+        if (sarah.child != null) {
+            System.out.println("sarah.child.getName() = " + sarah.child.getName());
+        } else {
+            System.out.println("No child ");
+        }
+        System.out.println("john.getName() = " + john.getName());
+        System.out.println("sarah.child.getName() = " + sarah.child.getName());
+        personService.changePersonToDani (sarah);
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String name;
+    private int age;
+    public Person parent;
+
+    public Person (String name, String age){
+        this.name  = name;
+        this age = age;
+    }
+public Person child;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge;
+
+    public Person(String sarah, int age = 30) {
+    }
+}
